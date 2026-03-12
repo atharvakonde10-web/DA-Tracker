827eca9d0df6a940977a3cba92ccb67bc2cbca42
Atharva Konde <atharvakonde10@gamil.com>
2026-03-12 16:48:15 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -7,17 +7,20 @@ const trackerRoutes = require("./routes/trackerRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET","POST"],
+  allowedHeaders: ["Content-Type"]
+}));
+
 app.use(express.json());
 
 app.use("/api/tracker", trackerRoutes);
 
-// MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
-// PORT fix for Render
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
