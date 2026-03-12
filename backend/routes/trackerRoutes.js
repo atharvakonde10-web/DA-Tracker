a4f8d8dbb107b877e588821d37d6e39ff624d83c
Atharva Konde <atharvakonde10@gamil.com>
2026-03-12 16:38:37 +0530

    fix schema for tracker data

--- backend/routes/trackerRoutes.js
+++ backend/routes/trackerRoutes.js
@@ -1,42 +1,15 @@
-const express = require("express");
-const router = express.Router();
-const Tracker = require("../models/Tracker");
+const mongoose = require("mongoose");
 
-// GET tracker
-router.get("/:userId", async (req, res) => {
-  try {
-    const tracker = await Tracker.findOne({ userId: req.params.userId });
-
-    res.json({ data: tracker ? tracker.data : {} });
-
-  } catch (err) {
-    res.status(500).json(err);
+const TrackerSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  data: {
+    type: mongoose.Schema.Types.Mixed,
+    default: {}
   }
 });
 
-// SAVE tracker
-router.post("/:userId", async (req, res) => {
-  try {
-    const { data } = req.body;
-
-    let tracker = await Tracker.findOne({ userId: req.params.userId });
-
-    if (tracker) {
-      tracker.data = data;
-    } else {
-      tracker = new Tracker({
-        userId: req.params.userId,
-        data
-      });
-    }
-
-    await tracker.save();
-
-    res.json({ success: true });
-
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-module.exports = router;
+module.exports = mongoose.model("Tracker", TrackerSchema);
